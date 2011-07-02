45ba35f196cb6d0d21d985640f526244627debf7
Oscar G.G <torrente76@yahoo.es>
2011-07-02 20:03:24 +0200

    Libro general creado

--- Tema 1 Introduccion/make.bat
+++ Tema 1 Introduccion/make.bat
@@ -1,1 +1,3 @@
-C:\Python26\Scripts\rst2s5.py Introduccion.txt > Introduccion.html
+#C:\Python27\Scripts\rst2s5.py Introduccion.txt > Introduccion.html
+/c/Python26/scripts/rst2s5.py Introduccion.txt > Introduccion.html
+/c/Python26/scripts/rst2s5.py S1-ESO.txt > S1-ESO.html
