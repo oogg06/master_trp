48291750cb5e96c3906cc3149c8669f9c6fb18fb
Oscar Gomez <torrente76@yahoo.es>
2011-08-20 22:21:29 +0200

    Nada que destacar

--- Tema 1 Introduccion/make.bat
+++ Tema 1 Introduccion/make.bat
@@ -1,1 +1,6 @@
-C:\Python26\Scripts\rst2s5.py Introduccion.txt > Introduccion.html
+# C:\Python26\Scripts\rst2s5.py Introduccion.txt > Introduccion.html
+
+/c/Archivos\ de\ programa/GnuWin32/bin/pic2plot.exe -f 0.03 esquema -T png > esquema.png
+/c/Python27/scripts/rst2s5.py Introduccion.txt > Introduccion.html
+
+/c/Python27/scripts/rst2s5.py S1-ESO.txt > S1-ESO.html
