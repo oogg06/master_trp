13f4cf104afccf24fe9e80c1d55a94f38009ae43
Oscar Gomez <torrente76@yahoo.es>
2011-08-23 16:04:04 +0200

    Completado el apartado de criterios Informatica en 4 de ESO

--- Tema 1 Introduccion/make.bat
+++ Tema 1 Introduccion/make.bat
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
 # C:\Python26\Scripts\rst2s5.py Introduccion.txt > Introduccion.html
 
-/c/Archivos\ de\ programa/GnuWin32/bin/pic2plot.exe -f 0.03 esquema -T png > esquema.png
+# /c/Archivos\ de\ programa/GnuWin32/bin/pic2plot.exe -f 0.03 esquema -T png > esquema.png
 /c/Python27/scripts/rst2s5.py Introduccion.txt > Introduccion.html
 
 /c/Python27/scripts/rst2s5.py S1-ESO.txt > S1-ESO.html
-=======
-#C:\Python27\Scripts\rst2s5.py Introduccion.txt > Introduccion.html
-/c/Python26/scripts/rst2s5.py Introduccion.txt > Introduccion.html
-/c/Python26/scripts/rst2s5.py S1-ESO.txt > S1-ESO.html
->>>>>>> 45ba35f196cb6d0d21d985640f526244627debf7
+
+
+# C:\Python27\Scripts\rst2s5.py Introduccion.txt > Introduccion.html
+# /c/Python26/scripts/rst2s5.py Introduccion.txt > Introduccion.html
+# /c/Python26/scripts/rst2s5.py S1-ESO.txt > S1-ESO.html
+
