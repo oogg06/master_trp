a29845dc2d89a8b676ea9bda70850d0bd110d223
Oscar G.G <torrente76@yahoo.es>
2011-10-27 21:07:04 +0200

    Desarrollo de los examenes LOE

--- Tema 1 Introduccion/make.bat
+++ Tema 1 Introduccion/make.bat
@@ -1,7 +1,7 @@
 #C:\Python26\Scripts\rst2s5.py Introduccion.txt > Introduccion.html
 #"c:\Archivos de programa\GnuWin32\bin\pic2plot.exe" -f 0.02 esquema -T png > esquema.png
 
-#/c/Python27/scripts/rst2s5.py Introduccion.txt > Introduccion.html
+#c/Python27/scripts/rst2s5.py Introduccion.txt > Introduccion.html
 
 #/c/Python27/scripts/rst2s5.py S1-ESO.txt > S1-ESO.html
 
