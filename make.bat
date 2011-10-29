635dbad6b406462f7253a94d514102d49af4eba0
Oscar Gomez <torrente76@yahoo.es>
2011-10-29 17:58:41 +0200

    Se incluye el PDF en el directorio Apuntes

---
+++ make.bat
@@ -0,0 +1,34 @@
+set DIR=apuntes
+
+
+del %DIR%\*.* /Q
+
+copy Apuntes.rst %DIR%
+
+
+copy T1-Intro\*.rst %DIR%
+copy T1-Intro\*.png %DIR%
+
+copy T2-ESO-PCPI\*.rst %DIR%
+copy T2-ESO-PCPI\*.png %DIR%
+
+copy T3-Bach\*.rst %DIR%
+copy T3-Bach\*.png %DIR%
+
+copy T4-CFGM\*.png %DIR%
+copy T4-CFGM\*.rst %DIR%
+
+copy T5-CFGS\*.png %DIR%
+copy T5-CFGS\*.rst %DIR%
+
+cd %DIR%
+"C:\Python27\Scripts\rst2newlatex.py" Apuntes.rst > Apuntes.tex
+pdflatex Apuntes.tex
+pdflatex Apuntes.tex
+start Apuntes.pdf
+del *.out
+del *.log
+del *.aux
+
+
+cd ..
