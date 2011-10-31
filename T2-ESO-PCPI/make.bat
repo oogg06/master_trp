586211db5b4c4fbd30042771f5bdbc860da38796
Oscar Gomez <torrente76@yahoo.es>
2011-10-31 23:09:17 +0100

    Inlcuidos los make

---
+++ T2-ESO-PCPI/make.bat
@@ -0,0 +1,42 @@
+SET IMGS="..\img"
+copy %IMGS%\esquema.png
+copy %IMGS%\SistemaEducativo.png
+copy %IMGS%\Atribuciones.png
+copy %IMGS%\Jerarquia.png
+
+set OBJ=T2
+
+set PYTHON="C:\Python27\scripts\asciidoc"
+
+set DIR_ICONOS="C:/Python27/scripts/asciidoc/images/icons"
+set ASC=%PYTHON%\asciidoc.py
+
+set OPCIONES_ICONOS=-a icons -a iconsdir=%DIR_ICONOS%
+
+set ASC2=%ASC% -a encoding=ISO-8859-1  -a lang=es %OPCIONES_ICONOS%  -a toc
+
+set BLATEX=%ASC2% -b latex
+
+set SLI=%ASC2% -b slidy
+
+%SLI% %OBJ%.rst
+
+
+%BLATEX% %OBJ%.rst
+
+
+
+pdflatex %OBJ%.tex
+pdflatex %OBJ%.tex
+
+del *.log
+del *.aux
+del *.idx
+del *.out
+del *.tex
+mkdir html
+move *.png html
+move *.html html
+start html\%OBJ%.html
+
+start %OBJ%.pdf
